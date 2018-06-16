1ec99de68dc713669a92432dbe5c1b49f3f85e63
jeetooo <jeetender.sharma100@gmail.com>
2018-06-16 12:20:35 +0530

    stripe

---
+++ public/javascripts/checkout.js
@@ -0,0 +1,97 @@
+Stripe.setPublishableKey('pk_test_cbyRm1bHWjGjdo5dh4Vfi7ev');
+
+$form = $("#checkout-form");
+
+$form.submit(function(){
+  $form.find("button").prop("disabled", true);
+  Stripe.card.createToken({
+    number: $('#card-number').val(),
+    cvc: $('#card-cvc').val(),
+    exp_month: $('#card-expiry-month').val(),
+    exp_year: $('#card-expiry-year').val()
+  }, stripeResponseHandler);
+  return false;
+});
+
+
+function stripeResponseHandler(status, response) {
+
+  if (response.error) { // Problem!
+
+    // Show the errors on the form
+    $form.find('#payment-errors').text(response.error.message).removeClass("hidden");
+    $form.find('button').prop('disabled', false); // Re-enable submission
+
+  } else { // Token was created!
+
+    $form.find('#payment-errors').empty().addClass("hidden");
+
+    // Get the token ID:
+    var token = response.id;
+
+    // Insert the token into the form so it gets submitted to the server:
+    $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+
+    // Submit the form:
+    $form.get(0).submit();
+
+  }
+}
+
+/*// Create a Stripe client.
+var stripe = Stripe('pk_test_cbyRm1bHWjGjdo5dh4Vfi7ev');
+
+// Create an instance of Elements.
+var elements = stripe.elements();
+
+// Custom styling can be passed to options when creating an Element.
+// (Note that this demo uses a wider set of styles than the guide below.)
+var style = {
+  base: {
+    color: '#32325d',
+    lineHeight: '18px',
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#aab7c4'
+    }
+  },
+  invalid: {
+    color: '#fa755a',
+    iconColor: '#fa755a'
+  }
+};
+
+// Create an instance of the card Element.
+var card = elements.create('card', {style: style});
+
+// Add an instance of the card Element into the `card-element` <div>.
+card.mount('#card-element');
+
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+// Handle form submission.
+var form = document.getElementById('checkout-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error.
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server.
+      stripeTokenHandler(result.token);
+    }
+  });
+});*/
--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,14 @@ var Product = require('../models/product');
 var Cart = require('../models/cart');
 
 router.get('/', function(req, res, next) {
+	var successMessage = req.flash("success")[0];
 	Product.find(function(err, products){
 		productChunks = [];
 		chunkSize = 3;
 		for (var i = 0; i < products.length; i += 3){
 			productChunks.push(products.slice(i, i + chunkSize));
 		}
-		res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
+		res.render('shop/index', { title: 'Shopping Cart', products: productChunks, successMessage: successMessage, noMessage: !successMessage});
 	});
 });
 
@@ -25,7 +26,7 @@ router.get('/add-to-cart/:id', function(req, res, next) {
 		}
 		cart.add(product, product.id);
 		req.session.cart = cart;
-		console.log(req.session.cart);
+		//console.log(req.session.cart);
 		res.redirect('/');
 	});
 });
@@ -35,7 +36,7 @@ router.get('/shopping-cart', function(req, res, next) {
 		return res.render('shop/shopping-cart', {products: null});
 	}
 	var cart = new Cart(req.session.cart);
-	res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice})
+	res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
 router.get('/checkout', function(req, res, next) {
@@ -43,7 +44,35 @@ router.get('/checkout', function(req, res, next) {
 		return res.render('/shopping-cart', {products: null});
 	}
 	var cart = new Cart(req.session.cart);
-	res.render('shop/checkout', {products: cart.generateArray(), totalPrice: cart.totalPrice})
+	var errMsg = req.flash("error")[0];
+	res.render('shop/checkout', {products: cart.generateArray(), totalPrice: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
+});
+
+router.post('/checkout', function(req, res, next) {
+	if(!req.session.cart){
+		return res.render('/shopping-cart', {products: null});
+	}
+	var cart = new Cart(req.session.cart);
+
+	var stripe = require("stripe")(
+	  "sk_test_W7i26YREGGHgfa2EaSH6ZXzP"
+	);
+
+	stripe.charges.create({
+	  amount: cart.totalPrice * 100,
+	  currency: "usd",
+	  source: req.body.stripeToken, // obtained with Stripe.js
+	  description: "Charge for stripe testing"
+	}, function(err, charge) {
+	  if(err){
+	  	req.flash("error", err.message);
+	  	return res.redirect("/checkout");
+	  }
+	  req.flash("success", "Successfully bought product!");
+	  req.session.cart = null;
+	  res.redirect("/");
+	});
+
 });
 
 module.exports = router;
